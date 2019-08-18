ddaed14ae1f7fb3a8a174a4dc573328fada48eee
Valentin Stoyanov <valkilmars@gmail.com>
2019-08-19 00:26:32 +0300

    Environment related...

--- app/config/app.php
+++ app/config/app.php
@@ -5,5 +5,5 @@ return [
     'url_polling_service' => 'https://mysterious-refuge-63151.herokuapp.com',
     
     // 'url_pusher_service' => 'http://localhost:3000/',
-    'url_pusher_service' => 'https://powerful-brushlands-87145.herokuapp.com:3000',
+    'url_pusher_service' => 'ws://powerful-brushlands-87145.herokuapp.com:3000',
 ];
