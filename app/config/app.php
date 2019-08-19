15175a6580d93a716cd265aed9fefe652635886a
Valentin Stoyanov <valkilmars@gmail.com>
2019-08-19 22:21:09 +0300

    Small css .glow fix

--- app/config/app.php
+++ app/config/app.php
@@ -4,6 +4,6 @@ return [
     // 'url_polling_service' => 'http://local.leaderboard.service',
     'url_polling_service' => 'https://mysterious-refuge-63151.herokuapp.com',
     
-    // 'url_pusher_service' => 'http://localhost:3000/',
+    // 'url_pusher_service' => 'http://localhost:3000',
     'url_pusher_service' => 'https://powerful-brushlands-87145.herokuapp.com',
 ];
