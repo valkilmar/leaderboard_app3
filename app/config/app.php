702d86cf32de27b94cbc6a0de68e6d18ddf0c513
Valentin Stoyanov <valkilmars@gmail.com>
2019-08-20 01:12:15 +0300

    Environment check and socket.io.js target path changed.

--- app/config/app.php
+++ app/config/app.php
@@ -1,9 +1,13 @@
 <?php
 
-return [
-    // 'url_polling_service' => 'http://local.leaderboard.service',
-    'url_polling_service' => 'https://mysterious-refuge-63151.herokuapp.com',
-    
-    // 'url_pusher_service' => 'http://localhost:3000',
-    'url_pusher_service' => 'https://powerful-brushlands-87145.herokuapp.com',
-];
+if (getenv('ENV') === 'heroku') {
+    return [
+        'url_polling_service' => 'https://mysterious-refuge-63151.herokuapp.com',
+        'url_pusher_service' => 'https://powerful-brushlands-87145.herokuapp.com',
+    ];
+} else {
+    return [
+        'url_polling_service' => 'http://local.leaderboard.service',
+        'url_pusher_service' => 'http://localhost:3000',
+    ];
+}
